58957d5fb3ee0a8f824c440e687643affa46749b
VencolSoft <niki.cesar2@gmail.com>
2018-10-21 12:49:39 -0500

    Big update 1.0 part2 autenticacion fix

--- app/src/main/java/com/vecolsoft/deligo_conductor/Splash/SplashActivity.java
+++ app/src/main/java/com/vecolsoft/deligo_conductor/Splash/SplashActivity.java
@@ -13,15 +13,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.leo.simplearcloader.ArcConfiguration;
 import com.leo.simplearcloader.SimpleArcDialog;
 import com.vecolsoft.deligo_conductor.Activitys.HomeBox;
 import com.vecolsoft.deligo_conductor.Activitys.MainActivity;
+import com.vecolsoft.deligo_conductor.Common.Common;
+import com.vecolsoft.deligo_conductor.Modelo.Driver;
 import com.vecolsoft.deligo_conductor.R;
 import com.vecolsoft.deligo_conductor.Utils.InternetConnection;
 import com.vecolsoft.deligo_conductor.Utils.Utils;
 
 public class SplashActivity extends AppCompatActivity {
+
+
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference ref;
 
     //verificar internet
     boolean connected = false;
@@ -42,6 +54,18 @@ public class SplashActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_splash);
 
+        //Firebase Views
+        auth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+
+            ref = database.getReference(Common.user_driver_tbl)
+                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        }
+
+
         //Dialogo de Carga
         final SimpleArcDialog CargaDialog = new SimpleArcDialog(this);
         ArcConfiguration configuration = new ArcConfiguration(this);
@@ -54,6 +78,8 @@ public class SplashActivity extends AppCompatActivity {
         CargaDialog.show();
 
         final Intent intentMain = new Intent(this, MainActivity.class);
+        final Intent intentHome = new Intent(this, HomeBox.class);
+        intentHome.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -67,10 +93,35 @@ public class SplashActivity extends AppCompatActivity {
                     //verificar si hay internet con ping
                     if (InternetConnection.internetIsConnected(c)) {
 
-                        startActivity(intentMain);
-                        CargaDialog.dismiss();
-                        finish();
-                        connected = true;
+                        if (auth.getCurrentUser() != null) {
+
+
+                            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                    Common.CurrentUser = dataSnapshot.getValue(Driver.class);
+                                    CargaDialog.dismiss();
+                                    startActivity(intentHome);
+                                    finish();
+                                    connected = true;
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+
+
+
+
+                        } else {
+                            CargaDialog.dismiss();
+                            startActivity(intentMain);
+                            finish();
+                        }
+
                     } else {
 
                         AlertDialog.Builder dialogo = new AlertDialog.Builder(c);
